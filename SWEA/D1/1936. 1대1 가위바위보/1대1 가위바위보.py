1a4165a41227c4aef09737908060a058ebe18cd1
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-15 17:18:23 +0900

    [D1] Title: 1대1 가위바위보, Time: 127 ms, Memory: 56,688 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+
+if a == 1:
+    if b == 2:
+        print("B")
+    else:
+        print("A")
+elif a == 2:
+    if b == 3:
+        print("B")
+    else:
+        print("A")
+elif a == 3:
+    if b == 1:
+        print("B")
+    else:
+        print("A")
