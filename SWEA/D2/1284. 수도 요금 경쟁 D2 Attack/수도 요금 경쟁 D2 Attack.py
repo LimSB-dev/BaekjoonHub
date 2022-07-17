ba9b31597a03b0bba2271257da1808b1ae9d9614
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-17 21:32:11 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 145 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -0,0 +1,10 @@
+t= int(input())
+for tc in range(1,t+1):
+  p, q, r, s, w = map(int, input().split())
+  a = p * w
+  if w <= r:
+    b = q
+  else:
+    b = q + ((w - r) * s)
+    
+  print(min(a, b))
