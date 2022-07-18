2a64c00f22fed06a3dc3f261bb60318eac655cd5
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-18 23:42:27 +0900

    [level 1] Title: 행렬의 덧셈, Time: 31.08 ms, Memory: 22.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,10 @@
+def solution(arr1, arr2):
+    answer = []
+    
+    for i in range(len(arr1)):
+        arr_sum = []
+        for j in range(len(arr1[0])):
+            arr_sum.append(arr1[i][j] + arr2[i][j])
+        answer.append(arr_sum)
+            
+    return answer
