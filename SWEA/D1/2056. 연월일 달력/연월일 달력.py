14cc427261c78fc4ecd77fdb3560184a37bb65aa
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-19 20:43:47 +0900

    [D1] Title: 연월일 달력, Time: 128 ms, Memory: 56,424 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.py
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.py
@@ -9,16 +9,20 @@ for tc in range(1,t+1):
     if int(month) in [1,3,5,7,8,10,12]:
         if int(day) < 1 or 31 < int(day):
             print(f'#{tc} -1')
-            
+            continue
+
     if int(month) in [4,6,9,11]:
         if int(day) < 1 or 30 < int(day):
             print(f'#{tc} -1')
-            
+            continue
+
     if int(month) == 2:
         if int(day) < 1 or 28 < int(day):
             print(f'#{tc} -1')
+            continue
 
     if int(month) < 1 or 12 < int(month):
         print(f'#{tc} -1')
+        continue
     
     print(f'#{tc} {year}/{month}/{day}')
