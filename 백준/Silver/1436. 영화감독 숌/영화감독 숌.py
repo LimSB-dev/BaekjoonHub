2c5fcc285eab52e3c6daa7f13dd841a8c5992491
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-21 15:24:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 196 ms, Memory: 124636 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+title_num = 666
+i = 0
+
+while True:
+    
+    if '666' in str(title_num):
+        i += 1
+        
+    if i == n:
+        print(title_num)
+        break
+        
+    title_num += 1
