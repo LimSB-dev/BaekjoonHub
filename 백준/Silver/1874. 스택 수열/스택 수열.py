d8986d2f0272d03b152d4c382ab562bcbf31fadb
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-23 20:44:08 +0900

    [Silver III] Title: 스택 수열, Time: 208 ms, Memory: 132468 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+li = []
+cnt = 0
+print_li = []
+X = True
+
+for _ in range(t):
+  n = int(input())
+
+  while cnt < n:
+    cnt += 1
+    li.append(cnt)
+    print_li.append('+')
+  
+  if li[-1] == n:
+    li.pop()
+    print_li.append('-')
+  
+  else:
+    X = False
+    break
+
+if X == False:
+  print('NO')
+
+else:
+  for i in print_li:
+    print(i)
