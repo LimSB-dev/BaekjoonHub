a00e826a964888ff26422362ab8e68d6d4c9c69a
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-25 13:20:38 +0900

    [Bronze V] Title: 학점계산, Time: 112 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,39 @@
+n = str(input())
+if n=='A+':
+  print(4.3)
+
+elif n=='A0':
+  print(4.0)
+ 
+elif n=='A-':
+  print(3.7)
+  
+elif n=='B+':
+  print(3.3)
+
+elif n=='B0':
+  print(3.0)
+  
+elif n=='B-':
+  print(2.7) 
+
+elif n=='C+':
+  print(2.3)
+
+elif n=='C0':
+  print(2.0)
+  
+elif n=='C-':
+  print(1.7)
+
+elif n=='D+':
+  print(1.3)
+
+elif n=='D0':
+  print(1.0)
+  
+elif n=='D-':
+  print(0.7)
+
+else:
+  print(0.0)
