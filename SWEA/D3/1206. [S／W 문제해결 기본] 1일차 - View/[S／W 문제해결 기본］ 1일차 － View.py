e7d23759580819fe7faa773678578494de87b762
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-28 20:45:08 +0900

    Delete SWEA/D3/1206. [S／W 문제해결 기본] 1일차 - View directory

--- SWEA/D3/1206. [S／W 문제해결 기본] 1일차 - View/[S／W 문제해결 기본］ 1일차 － View.py
+++
@@ -1,9 +0,0 @@
-t = 10
-for tc in range(1,t+1):
-  lenght = int(input())
-  li = list(map(int, input().split()))
-  result = 0
-  for i in range(2, lenght - 2):
-    if li[i] > li[i+1] and li[i] > li[i+2] and li[i] > li[i-1] and li[i] > li[i-2]:
-      result += li[i] - max(li[i-2],li[i-1],li[i+1],li[i+2])
-  print(f"#{tc} {result}")
