e209a1a5c3ab060bea52adc8ed1524b51eb04e18
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-07-29 00:12:26 +0900

    [Gold IV] Title: 검문, Time: 2344 ms, Memory: 124504 KB -BaekjoonHub

--- 백준/Gold/2981. 검문/검문.py
+++ 백준/Gold/2981. 검문/검문.py
@@ -1,3 +1,25 @@
-from math import*;w=n=p=d=0;b=[];exec('q=int(input());w=w or q;n=gcd(n,q-w);'*int(input()))
-while~d*~d<=n:d+=1;b+=(n%d<1)*[d,n//d]
-print(*sorted({*b})[1:])
+import sys
+input = sys.stdin.readline
+
+def isGCD(num1,num2):
+  while num2 != 0:
+    num1,num2 = num2,(num1 % num2)
+  return num1
+
+n = int(input())
+li = []
+
+for i in range(n):
+  num = int(input())
+  li.append(num)
+li.sort()
+
+gcd = li[1] - li[0]
+
+for i in range(2,n):
+  gcd = isGCD(gcd,li[i] - li[i-1])
+
+for i in range(2,gcd//2+1):
+  if gcd % i == 0:
+    print(i,end=' ')
+print(gcd)
