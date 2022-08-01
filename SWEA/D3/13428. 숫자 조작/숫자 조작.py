6717342c1983234f362ea26cbcd56c2df9012ab0
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-01 20:21:24 +0900

    [D3] Title: 숫자 조작, Time: 148 ms, Memory: 56,944 KB -BaekjoonHub

--- SWEA/D3/13428. 숫자 조작/숫자 조작.py
+++ SWEA/D3/13428. 숫자 조작/숫자 조작.py
@@ -1,16 +1,42 @@
-from itertools import combinations
+# 최소값 찾기
+def min_val(n):
+    li = list(n)
+    for i in range(len(li)-1):
+        min_idx = i
+        for j in range(i + 1, len(li)):
+            if li[i] > li[j]:
+                if i != 0:
+                    if li[min_idx] >= li[j]:
+                        min_idx = j
+                else:
+                    if li[min_idx] >= li[j] and li[j] != '0':
+                        min_idx = j
+
+        if i != min_idx and li[i] != li[min_idx]:
+            li[i], li[min_idx] = li[min_idx], li[i]
+            return ''.join(li)
+        
+    return ''.join(li)
+
+# 최대값 찾기
+def max_val(n):
+    li = list(n)
+    for i in range(len(li)-1):
+        max_idx = i
+        for j in range(i + 1, len(li)):
+            if li[i] < li[j]:
+                if li[max_idx] <= li[j]:
+                    max_idx = j
+
+        if i != max_idx and li[i] != li[max_idx]:
+            li[i], li[max_idx] = li[max_idx], li[i]
+            return ''.join(li)
+
+    return ''.join(li)
 
 t = int(input())
-
-for tc in range(1, t + 1):
-    n = list(input())
-    tmp = [int("".join(n))]
-
-    for a, b in combinations(range(len(n)), 2):
-        n[a], n[b] = n[b], n[a]
-
-        if n[0] != "0":
-            tmp.append(int("".join(n)))
-        n[b], n[a] = n[a], n[b]
-
-    print(f"#{tc} {min(tmp)} {max(tmp)}")
+for tc in range(1, t+1):
+    n = input()
+    minV = min_val(n)
+    maxV = max_val(n)
+    print(f"#{tc} {minV} {maxV}")
