6612ecaf1a06cd2cf08e47c1a607845917f47351
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-02 22:30:13 +0900

    [Silver III] Title: 두 수의 합, Time: 116 ms, Memory: 46476 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,25 +1,8 @@
-import sys
-
-input = sys.stdin.readline
-print = sys.stdout.write
-
-n = int(input().rstrip())
-
-li = sorted(list(map(int, input().split())))
-
-x = int(input().rstrip())
-
-start,end=0,n-1
+n=int(input())
+s=set(map(int,input().split()))
+x=int(input().strip())
 cnt = 0
-
-while start < end:
-  if li[start] + li[end] == x:
-    cnt += 1
-    start += 1
-    end -= 1
-  elif li[start] + li[end] < x:
-    start += 1
-  elif li[start] + li[end] > x:
-    end -= 1
-
-print(f'{cnt}')
+for i in s:
+    if x - i in s:
+        cnt += 1
+print(cnt//2)
