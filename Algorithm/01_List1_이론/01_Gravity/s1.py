01e076a9d10d4a9685f97313ddfb960ec177549d
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-09 20:58:43 +0900

    Algorithm is Fun

--- Algorithm/01_List1_이론/01_Gravity/s1.py
+++ Algorithm/01_List1_이론/01_Gravity/s1.py
@@ -1,10 +1,32 @@
 import sys
-
 sys.stdin = open('input.txt')
 
 t = int(input())
 
 for tc in range(1, t + 1):
+    n = int(input())
+    arr = list(map(int, input().split()))
     answer = 0
+    max_height = arr[0]
 
-    print(f'{tc} {answer}')
+    for i in range(n):
+        # 중력 크기를 담을 변수 gravity에 초기값 0을 할당한다.
+        gravity = 0
+
+        # 만약 index가 i인 상자의 높이가 현재 최대 높이보다 높다면 max_height에 li[i]를 대입시킨다.
+        # 오른쪽 상자의 높이가 이전의 가장 높은 상자에 비해 낮다면 굳이 탐색할 필요가 없다.
+        if max_height <= arr[i]:
+            max_height = arr[i]
+
+            # 오른쪽 상자부터 끝까지 탐색한다.
+            for j in range(i + 1, n):
+
+                # 만약 i index의 상자가 j index 상자보다 높다면 아래로 낙하하기 때문에 gravity가 1 증가한다.
+                if arr[i] > arr[j]:
+                    gravity += 1
+
+                # 만약 gravity 크기가 answer보다 크다면 answer에 gravity 대입
+                if gravity > answer:
+                    answer = gravity
+
+    print(f'#{tc} {answer}')
--- Algorithm/01_List1_이론/02_Baby_gin/s1.py
+++ Algorithm/01_List1_이론/02_Baby_gin/s1.py
@@ -1,10 +1,41 @@
 import sys
-
-sys.stdin = open('input.txt')
+sys.stdin = open("input.txt")
 
 t = int(input())
 
 for tc in range(1, t + 1):
+    arr = list(map(int, input()))
     answer = 0
+    # 0 ~ 9 까지의 숫자 범위이고 out of range ERROR를 피하기 위해 범위보다 많은 저장 공간을 만든다.
+    count_arr = [0] * 12
+    run = triple = 0
 
-    print(f'{tc} {answer}')
+    # count_arr의 값을 채워준다.
+    for i in range(6):
+        count_arr[arr[i]] += 1
+
+    # count_arr의 요소를 탐색한다.
+    for i in range(12):
+
+        # 만약 index의 값이 3이상으로 triple일 경우
+        if count_arr[i] >= 3:
+            triple += count_arr[i] // 3
+            count_arr[i] %= 3
+
+        # 만약 연속된 3개의 index의 값이 모두 0보다 클 경우
+        if count_arr[i] > 0 and count_arr[i+1] > 0 and count_arr[i+2] > 0:
+            run += 1
+            count_arr[i] -= 1
+            count_arr[i+1] -= 1
+            count_arr[i+2] -= 1
+            # 같은 연속된 수의 run일 경우 한 번 더 조건을 만족할 수 있다.
+            if count_arr[i] > 0 and count_arr[i+1] > 0 and count_arr[i+2] > 0:
+                run += 1
+                count_arr[i] -= 1
+                count_arr[i+1] -= 1
+                count_arr[i+2] -= 1
+
+    if triple + run == 2:
+        answer = 1
+
+    print(f'#{tc} {answer}')
