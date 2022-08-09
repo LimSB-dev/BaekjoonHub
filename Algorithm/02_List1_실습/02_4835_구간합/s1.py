5f9db40610e5b95245c5ea9e1996eb037903f1bd
LimSB-dev <mae03087@naver.com>
2022-08-09 12:57:59 +0900

    Algorithm is Fun

--- Algorithm/02_List1_실습/02_4835_구간합/s1.py
+++ Algorithm/02_List1_실습/02_4835_구간합/s1.py
@@ -1,10 +1,25 @@
-import sys
-
-sys.stdin = open('input.txt')
-
 t = int(input())
 
 for tc in range(1, t + 1):
     answer = 0
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    max_value = 0
+    min_value = 0
+    for j in range(m):
+        max_value += arr[j]
+        min_value += arr[j]
 
-    print(f'{tc} {answer}')
+    for i in range(n - m + 1):
+        sum_value = 0
+        for j in range(m):
+            sum_value += arr[i + j]
+
+        if sum_value > max_value:
+            max_value = sum_value
+        elif sum_value < min_value:
+            min_value = sum_value
+
+    answer = max_value - min_value
+
+    print(f'#{tc} {answer}')
--- Algorithm/02_List1_실습/05_4824_숫자_카드/s1.py
+++ Algorithm/02_List1_실습/05_4824_숫자_카드/s1.py
@@ -4,14 +4,20 @@ for tc in range(1, t + 1):
     n = int(input())
     numbers = input()
     answer = 0
+    # 카운팅 정렬을 담을 리스트 생성
     count_arr = [0] * 10
 
+    # 문자열 numbers를 순회하면서
     for number in numbers:
+        # 카운팅 정렬의 int(number)에 해당하는 인덱스에 1을 추가
         count_arr[int(number)] += 1
 
-    max_number = count_arr[0]
+    # 가장 많은 카드에 적힌 숫자
     index = 0
+    # 그 카드가 몇 장인지
+    max_number = count_arr[0]
 
+    # count_arr의 0 ~ 9 까지 index를 순회
     for i in range(10):
         if count_arr[i] >= max_number:
             max_number = count_arr[i]
