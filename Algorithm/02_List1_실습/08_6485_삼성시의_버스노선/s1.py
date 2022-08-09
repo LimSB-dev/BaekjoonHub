9a3f8fbe2ab1b8928930ce6b408f3ae32ef4eda8
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 00:42:43 +0900

    Test - Changing var name

--- Algorithm/02_List1_실습/08_6485_삼성시의_버스노선/s1.py
+++ Algorithm/02_List1_실습/08_6485_삼성시의_버스노선/s1.py
@@ -5,10 +5,10 @@ t = int(input())
 
 for tc in range(1, t + 1):
     n = int(input())
-    station_arr = [[0, 0] for _ in range(n)]
+    stations = [[0, 0] for _ in range(n)]
     for i in range(n):
         a, b = map(int, input().split())
-        station_arr[i] = [a, b]
+        stations[i] = [a, b]
 
     p = int(input())
 
@@ -16,7 +16,7 @@ for tc in range(1, t + 1):
     for i in range(p):
         station = int(input())
         for j in range(n):
-            if station_arr[j][0] <= station <= station_arr[j][1]:
+            if stations[j][0] <= station <= stations[j][1]:
                 answer_arr[i] += 1
 
     print(f'#{tc}', end=' ')
