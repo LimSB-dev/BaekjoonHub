35152618e5106879dc050f414334a98c9c5d1692
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 23:53:27 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,7 +1,15 @@
 def solution(s):
+    # 가운데 글자를 반환받을 변수
     answer = ''
+    
+    # 만약 단어의 길이가 홀수라면
     if len(s) % 2:
+        # 가운데 한글자를 answer에 대입
         answer = s[len(s)//2]
+        
+    # 만약 단어의 길이가 짝수라면
     else:
+        # 가운데 두글자를 answer에 대입
         answer = s[len(s)//2-1:len(s)//2+1]
+        
     return answer
