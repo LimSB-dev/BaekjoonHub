b3de67a71565aa99a1674ec902979a92c113a598
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 23:51:09 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.44 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,6 +1,10 @@
 def solution(numbers):
     answer = []
+    # list out of range를 피해기 위해 범위에 -1
     for i in range(len(numbers) - 1):
+        # i 이후의 값을 더하기 위해 i + 1을 범위의 시작으로 한다.
         for j in range(i + 1, len(numbers)):
             answer.append(numbers[i]+numbers[j])
+            
+    # 리스트 answer에서 중복되는 값을 제거 후 리스트로 변환한 뒤 오름차순으로 정렬 
     return sorted(list(set(answer)))
