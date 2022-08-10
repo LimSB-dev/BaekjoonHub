949f40c9a1a957bc8f7cd93992c1c4cf77b83072
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 19:22:43 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+li = []
+
+for _ in range(9):
+    n = int(input())
+    li.append(n)
+
+for i in combinations(li, 7):
+    if sum(i) == 100:
+        anw = list(i)
+        break
+        
+anw.sort()
+
+for i in anw:
+  print(i)
