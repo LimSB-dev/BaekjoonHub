01850bd7da9548d18b315964d3d2c7cfcae20cd5
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 22:45:16 +0900

    [D2] Title: 숫자 배열 회전, Time: 138 ms, Memory: 56,940 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -4,9 +4,15 @@ for tc in range(1, t + 1):
     n = int(input())
     # 2차원 배열 생성
     matrix = [list(map(str, input().split())) for _ in range(n)]
+
+    # 90도 회전한 숫자열을 담을 배열 answer 생성
     answer = [[''] * 3 for _ in range(n)]
+
+    # 2 중 for 문을 통한 matrix 탐색
     for row in range(n):
         for col in range(n):
+
+            # 아래는 직접 손으로 적어가면서 찾은 규칙을 기반으로 구현했다.
             answer[row][0] += matrix[n - 1 - col][row]
             answer[row][1] += matrix[n - 1 - row][n - 1 - col]
             answer[row][2] += matrix[col][n - 1 - row]
