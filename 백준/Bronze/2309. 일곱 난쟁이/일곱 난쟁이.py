27261d6a9e02471eb0cfe9398fcb7756ba5df41a
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 21:04:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,17 +1,24 @@
-from itertools import combinations
+# 난쟁이의 수 = 9, 일곱 난쟁이 키의 합 = 100으로 고정이지만 확장성을 위해 변수 t, sum_height 생성
+t = 9
+sum_height = 100
 
-li = []
+heights = [0] * t
 
-for _ in range(9):
+for i in range(t):
     n = int(input())
-    li.append(n)
+    heights[i] += n
 
-for i in combinations(li, 7):
-    if sum(i) == 100:
-        anw = list(i)
+for i in range(1 << t):
+    seven_height = 0
+    answer = []
+    for j in range(t):
+        if i & (1 << j):
+            seven_height += heights[j]
+            answer.append(heights[j])
+
+    if seven_height == sum_height and len(answer) == 7:
+        answer.sort()
         break
-        
-anw.sort()
 
-for i in anw:
-  print(i)
+for i in answer:
+    print(i)
