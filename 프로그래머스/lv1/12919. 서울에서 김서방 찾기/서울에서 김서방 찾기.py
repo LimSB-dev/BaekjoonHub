ef2e50c129b8669051ec5cb29917bd801a6bf144
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-10 23:58:16 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,5 +1,9 @@
 def solution(seoul):
+    # enumerate를 사용해 index값과 value값을 가져온다.
     for index, value in enumerate(seoul):
+        
+        # value값이 문제의 조건인 'Kim'이라면
         if value == 'Kim':
-            answer = f'김서방은 {index}에 있다'
-    return answer
+            
+            # 변수에 대입할 필요 없이 바로 반환
+            return f'김서방은 {index}에 있다'
