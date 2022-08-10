819f9515c06b92a753e4471247acd9ff15065a02
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-11 00:32:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,26 +1,25 @@
 def solution(s):
-    li = []
-    count = 0
-    for i in s:
-        if i == ' ':
-            li.append(i)
-            count = 0
-        else:
-            if not li:
-                if count % 2 == 0:
-                    li.append(i.upper())
-                else:
-                    li.append(i.lower())
-            elif i != ' ':
-                if count % 2 == 0:
-                    li.append(i.upper())
-                else:
-                    li.append(i.lower())
-            count += 1                
-    
+    # 정답을 입력 받을 문자열 answer
     answer = ''
     
-    for i in li:
-        answer += i
+    # 글자의 index값을 홀수와 짝수 구분하여 소문자 및 대문자로 변환시키기 위한 변수
+    cnt = 0
+    for i in s:
+
+        # 공백이라면 cnt를 0으로 초기화 후 반복문을 처음부터 다시
+        if i == ' ':
+            answer += i
+            cnt = 0
+            continue
+            
+        # 홀수라면 소문자 추가
+        if cnt % 2:
+            answer += i.lower()
+        # 짝수라면 대문자 추가
+        else:
+            answer += i.upper()
+            
+        # 글자의 index값 1씩 증가
+        cnt += 1                
     
     return answer
