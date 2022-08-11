5d5f59ffb4668d54b6e96b36c5cf07d5ec7c9244
LimSB-dev <mae03087@naver.com>
2022-08-11 11:03:31 +0900

    [D2] Title: 숫자를 정렬하자, Time: 138 ms, Memory: 57,180 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -2,7 +2,13 @@ t = int(input())
 
 for tc in range(1, t + 1):
     n = int(input())
-    li = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
+
+    # 버블 탐색
+    for i in range(n-1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     print(f'#{tc}', end=' ')
-    print(*sorted(li))
+    print(*arr)
