820f5102f64e16f7ec3cd6ea39ca0223de4bd93a
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-12 21:07:21 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.61 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,79 +1,40 @@
 def solution(numbers, hand):
     answer = ''
-    
-    left_row = 3
-    left_col = 0
-    
-    right_row = 3
-    right_col = 2
-    
-    # 번호 누르기
+    key_dict = {1:(0,0),2:(0,1),3:(0,2),
+                4:(1,0),5:(1,1),6:(1,2),
+                7:(2,0),8:(2,1),9:(2,2),
+                '*':(3,0),0:(3,1),'#':(3,2)}
+
+    left = [1,4,7]
+    right = [3,6,9]
+    lhand = '*'
+    rhand = '#'
     for i in numbers:
-        # 0일 경우
-        if i == 0:
-            # 0과 왼손 거리
-            left_distance = (3 - left_row) + (1 - left_col)
-            # 0과 오른손 거리
-            right_distance = (3 - right_row) + (right_col - 1)
-                        
-            if left_distance > right_distance:
-                right_row = 3
-                right_col = 1
-                answer += 'R'
-            elif left_distance < right_distance:
-                left_row = 3
-                left_col = 1
-                answer += 'L'
-            else:
-                if hand == 'right':
-                    right_row = 3
-                    right_col = 1
-                    answer += 'R'
-                else:
-                    left_row = 3
-                    left_col = 1
-                    answer += 'L'
-        # 0이 아닐 경우
+        if i in left:
+            answer += 'L'
+            lhand = i
+        elif i in right:
+            answer += 'R'
+            rhand = i
         else:
-            # 열 찾기
-            col = i % 3
-            # 오른손
-            if col == 0:
-                right_row = (i//3) - 1
-                right_col = (i%3) - 1
-                if right_col == -1:
-                    right_col = 2
-                answer += 'R'
-            # 왼손
-            elif col == 1:
-                left_row = i // 3
-                left_col = (i%3) - 1
+            curPos = key_dict[i]
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])
+            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
+
+            if ldist < rdist:
                 answer += 'L'
-            # 중앙
+                lhand = i
+            elif ldist > rdist:
+                answer += 'R'
+                rhand = i
             else:
-                i_row = i // 3
-                i_col = (i%3) - 1
-                
-                # 왼손 거리
-                left_distance = abs(i_row - left_row) + (i_col - left_col)
-                # 오른손 거리
-                right_distance = abs(i_row - right_row) + (right_col - i_col)
-                
-                if left_distance > right_distance:
-                    right_row = i_row
-                    right_col = i_col
-                    answer += 'R'
-                elif left_distance < right_distance:
-                    left_row = i_row
-                    left_col = i_col
+                if hand == 'left':
                     answer += 'L'
+                    lhand = i
                 else:
-                    if hand == 'right':
-                        right_row = i_row
-                        right_col = i_col
-                        answer += 'R'
-                    else:
-                        left_row = i_row
-                        left_col = i_col
-                        answer += 'L'
+                    answer += 'R'
+                    rhand = i
+
     return answer
