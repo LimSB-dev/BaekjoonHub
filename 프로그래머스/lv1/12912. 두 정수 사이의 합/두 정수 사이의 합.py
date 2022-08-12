c0ea69b635cab56a3d7480c57297bff138455449
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-12 21:17:49 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,6 +1,9 @@
 def solution(a, b):
+    # a, b 값들 중 무엇이 더 큰지 알지 모르기에 abs()를 사용
+    # 가우스의 덧셈정리를 사용
     answer = ((a + b) / 2) * (abs(a - b) + 1)
     
+    # a, b가 같은 경우 아무거나 출력
     if a == b:
         answer = a
         
