4f85d1870a6e697f4ef732b3e56a6e64c717b090
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-13 00:40:38 +0900

    [D1] Title: 점점 커지는 당근의 개수, Time: 128 ms, Memory: 56,936 KB -BaekjoonHub

---
+++ SWEA/D1/9367. 점점 커지는 당근의 개수/README.md
@@ -0,0 +1,11 @@
+# [D1] 점점 커지는 당근의 개수 - 9367 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AW_nY2m6OLADFARY) 
+
+### 성능 요약
+
+메모리: 56,936 KB, 시간: 128 ms, 코드길이: 441 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/9367. 점점 커지는 당근의 개수/점점 커지는 당근의 개수.py
@@ -0,0 +1,18 @@
+for tc in range(1, int(input()) + 1):
+    answer = 1
+    max_value = 1
+    n = int(input())
+    numbers = list(map(int, input().split()))
+    pre_num = numbers[0]
+
+    for number in numbers:
+        if number > pre_num:
+            answer += 1
+            pre_num = number
+            if max_value < answer:
+                max_value = answer
+        else:
+            pre_num = number
+            answer = 1
+
+    print(f'#{tc} {max_value}')
