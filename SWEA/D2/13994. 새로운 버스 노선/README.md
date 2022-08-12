41d2211eb2cea0bac656e94f7ce256f7876f226b
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-13 01:49:32 +0900

    [D2] Title: 새로운 버스 노선, Time: 205 ms, Memory: 61,968 KB -BaekjoonHub

---
+++ SWEA/D2/13994. 새로운 버스 노선/README.md
@@ -0,0 +1,11 @@
+# [D2] 새로운 버스 노선 - 13994 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AX875Xm6ABoDFAQe) 
+
+### 성능 요약
+
+메모리: 61,968 KB, 시간: 205 ms, 코드길이: 1,855 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/13994. 새로운 버스 노선/새로운 버스 노선.py
@@ -0,0 +1,57 @@
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    counter = [0] * 1001
+
+    for _ in range(n):
+        bus = list(map(int, input().split()))
+
+        # 일반
+        if bus[0] == 1:
+            for i in range(bus[1], bus[2] + 1):
+                counter[i - 1] += 1
+
+        # 급행
+        elif bus[0] == 2:
+            for i in range(bus[1], bus[2] + 1):
+                # 짝수
+                if bus[1] % 2 == 0:
+                    if i == bus[1]:
+                        counter[i - 1] += 1
+                    # 마지막 정거장은 필수
+                    elif i == bus[2]:
+                        counter[i - 1] += 1
+                    elif i % 2 == 0:
+                        counter[i - 1] += 1
+                # 홀수
+                else:
+                    if i == bus[1]:
+                        counter[i - 1] += 1
+                    # 마지막 정거장은 필수
+                    elif i == bus[2]:
+                        counter[i - 1] += 1
+                    elif i % 2 == 1:
+                        counter[i - 1] += 1
+
+        # 광역 급행
+        else:
+            for i in range(bus[1], bus[2] + 1):
+                # 짝수
+                if bus[1] % 2 == 0:
+                    if i == bus[1]:
+                        counter[i - 1] += 1
+                    # 마지막 정거장은 필수
+                    elif i == bus[2]:
+                        counter[i - 1] += 1
+                    elif i % 4 == 0:
+                        counter[i - 1] += 1
+                # 홀수
+                else:
+                    if i == bus[1]:
+                        counter[i - 1] += 1
+                    # 마지막 정거장은 필수
+                    elif i == bus[2]:
+                        counter[i - 1] += 1
+                    elif i % 3 == 0 and i % 10 != 0:
+                        counter[i - 1] += 1
+
+    print(f'#{tc} {max(counter)}')
