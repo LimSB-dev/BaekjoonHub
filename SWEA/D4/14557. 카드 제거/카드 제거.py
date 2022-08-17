dec9d561e1205d1545313fc48306fe71b882a74f
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-17 20:05:19 +0900

    [D4] Title: 카드 제거, Time: 354 ms, Memory: 58,536 KB -BaekjoonHub

--- SWEA/D4/14557. 카드 제거/카드 제거.py
+++ SWEA/D4/14557. 카드 제거/카드 제거.py
@@ -1,2 +1,2 @@
-for t in range(1, int(input()) + 1):
-    print(f'#{t} {"yes" if input().count("1") % 2 == 1 else "no"}')
+for t in range(int(input())):
+    print(f'#{t+1} {"yes" if input().count("1")%2 else "no"}')
