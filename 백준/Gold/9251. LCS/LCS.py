7084a39a069e65c05f997d94d437a6b347d939a5
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-17 22:54:12 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 122356 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,19 @@
+words = []
+
+for _ in range(2):
+  words.append(list(map(str,input().rstrip())))
+
+a = len(words[0])
+b = len(words[1])
+
+dp = [[0]*(b+1) for _ in range(a+1)]
+
+for i in range(1,a+1):
+  for j in range(1,b+1):
+    if words[0][i-1] == words[1][j-1]:
+      dp[i][j] = dp[i-1][j-1] + 1
+    else:
+      dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+      continue
+
+print(dp[a][b])
