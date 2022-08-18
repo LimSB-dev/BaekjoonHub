7a2c6d3f4a9ce9a8b4cee50c9e482111e605f7ee
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-18 19:47:28 +0900

    [D4] Title: 자기 방으로 돌아가기, Time: 165 ms, Memory: 61,964 KB -BaekjoonHub

---
+++ SWEA/D4/4408. 자기 방으로 돌아가기/README.md
@@ -0,0 +1,11 @@
+# [D4] 자기 방으로 돌아가기 - 4408 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWNcJ2sapZMDFAV8) 
+
+### 성능 요약
+
+메모리: 61,964 KB, 시간: 165 ms, 코드길이: 589 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
@@ -0,0 +1,31 @@
+def even_odd(num):
+    if num % 2 == 0:
+        num //= 2
+        return num
+    else:
+        num //= 2
+        num += 1
+        return num
+
+
+for tc in range(1, int(input()) + 1):
+    answer = 0
+    n = int(input())
+
+    counter = [0] * 201
+
+    for i in range(n):
+        room1, room2 = map(int, input().split())
+
+        room1 = even_odd(room1)
+        room2 = even_odd(room2)
+
+        if room1 > room2:
+            room1, room2 = room2, room1
+
+        for corridor in range(room1, room2 + 1):
+            counter[corridor] += 1
+
+    answer = max(counter)
+
+    print(f'#{tc} {answer}')
