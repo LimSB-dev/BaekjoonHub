aa12d4856cbd271d22498afb098c0a1d1f9d04df
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-18 19:51:21 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = []
+
+    # 문자 s를 리스트로 변경
+    s = list(s[2:-2].split('},{'))
+    
+    # 집합의 길이 순으로 정렬
+    sorted_s = sorted(s, key = lambda x: len(x))
+    
+    # 요소의 가장 마지막 값을 반환
+    for i in sorted_s:
+        # answer이 비어있지 않을 경우
+        if answer:
+            i = i.split(',')
+            for j in i:
+                if int(j) in answer:
+                    continue
+                else:
+                    answer.append(int(j))
+        else:
+            answer.append(int(i))
+    
+    return answer
