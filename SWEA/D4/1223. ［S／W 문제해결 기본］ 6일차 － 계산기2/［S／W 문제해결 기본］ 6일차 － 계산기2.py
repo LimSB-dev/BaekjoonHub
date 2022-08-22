3efd4508f752f4f9e8fb4be3a0eba7fb8c3e6430
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-22 21:28:02 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 161 ms, Memory: 56,704 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -1,7 +1,7 @@
-operator = ['(', ')', '+', '-', '*', '/']
-
 for tc in range(1, 11):
+    # 테스트 케이스의 길이
     n = int(input())
+
     # 중위표기법 입력
     arr = list(input())
 
@@ -13,39 +13,27 @@ for tc in range(1, 11):
 
     for i in arr:
         # 연산자 및 괄호
-        if i in operator:
+        if i in '+*':
 
-            # stack이 비어있거나 여는 괄호
-            if not stack or i == '(':
-                stack.append(i)
-
-            # * 혹은 /
-            elif i in '*/':
+            # *
+            if i in '*':
 
                 # stack이 비거나 stack의 top 우선순위가 낮을 때까지 반복
-                while stack and stack[-1] in '*/':
+                while stack and stack[-1] in '*':
                     # 스택에서 pop
                     answer += stack.pop()
 
                 stack.append(i)
 
-            # + 혹은 -
-            elif i in '+-':
+            # +
+            elif i in '+':
 
                 # stack이 비거나 stack의 top이 여는 괄호일 때까지 반복
-                while stack and stack[-1] != '(':
+                while stack:
                     # 스택에서 pop
                     answer += stack.pop()
 
                 stack.append(i)
-
-            # 닫는 괄호
-            elif i == ')':
-                # 여는 괄호일 때까지 반복
-                while stack[-1] != '(':
-                    answer += stack.pop()
-
-                stack.pop()
 
         # 피연산자
         else:
@@ -55,25 +43,21 @@ for tc in range(1, 11):
     while stack:
         answer += stack.pop()
 
-    # 후위표기법 계산기
+    # stack 초기화
     stack = []
+
+    # 후위표기법 계산기
     for i in answer:
         # 연산자
-        if i in '+-*/':
+        if i in '+*':
             a = stack.pop()
             b = stack.pop()
 
             if i == '+':
                 c = b + a
 
-            elif i == '-':
-                c = b - a
-
             elif i == '*':
                 c = b * a
-
-            elif i == '/':
-                c = b / a
 
             stack.append(c)
 
@@ -81,6 +65,4 @@ for tc in range(1, 11):
         else:
             stack.append(int(i))
 
-    answer = stack[-1]
-
-    print(f'#{tc} {answer}')
+    print(f'#{tc} {stack.pop()}')
