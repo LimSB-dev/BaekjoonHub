1535f108c714ccc82dc50f0e3a2649fe4ef8926d
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-23 22:29:32 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 362 ms, Memory: 73,016 KB -BaekjoonHub

---
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/README.md
@@ -0,0 +1,11 @@
+# [D4] 격자판의 숫자 이어 붙이기 - 2819 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7I5fgqEogDFAXB) 
+
+### 성능 요약
+
+메모리: 73,016 KB, 시간: 362 ms, 코드길이: 578 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -0,0 +1,29 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def dfs(x, y, words):
+    words += matrix[y][x]
+
+    if len(words) == 7:
+        answer.append(words)
+        return
+
+    for direction in range(4):
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            dfs(nx, ny, words)
+
+
+for tc in range(1, int(input()) + 1):
+    matrix = [list(input().split()) for _ in range(4)]
+    answer = []
+
+    for row in range(4):
+        for col in range(4):
+            dfs(col, row, '')
+
+    answer = set(answer)
+
+    print(f'#{tc} {len(answer)}')
