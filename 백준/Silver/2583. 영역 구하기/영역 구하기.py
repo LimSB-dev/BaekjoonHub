6348e5dcedfb352879d20d0eaa21d3629362aba3
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-23 22:36:23 +0900

    [Silver I] Title: 영역 구하기, Time: 92 ms, Memory: 40744 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,41 @@
+import sys
+sys.setrecursionlimit(100000)
+
+dx = [-1, 1, 0, 0]
+dy = dx[::-1]
+
+
+def dfs(x, y):
+    visited[y][x] = True
+    for direction in range(4):
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 0 <= nx < w and 0 <= ny < h and not visited[ny][nx]:
+            global space
+            space += 1
+            dfs(nx, ny)
+
+
+w, h, k = map(int, input().split())
+visited = [[False] * w for _ in range(h)]
+answer = 0
+arr = []
+
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for row in range(x1, x2):
+        for col in range(y1, y2):
+            visited[row][col] = True
+
+for row in range(h):
+    for col in range(w):
+        if not visited[row][col]:
+            space = 1
+            answer += 1
+            dfs(col, row)
+            arr.append(space)
+
+arr.sort()
+
+print(answer)
+print(*arr)
