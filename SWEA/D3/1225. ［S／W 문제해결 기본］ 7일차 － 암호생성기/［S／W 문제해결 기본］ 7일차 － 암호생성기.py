30698bc6376dd97d4f6c19cfd3854e6ba9b1d9a2
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-24 13:03:40 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 156 ms, Memory: 60,356 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,21 +1,20 @@
 for _ in range(1, 11):
     tc = int(input())
-    arr = list(map(int, input().split()))
-    answer = []
+    q = list(map(int, input().split()))
 
     # 0 이하의 값이 나올 때까지 반복
-    while arr[-1] != 0:
+    while q[-1] != 0:
 
         # 사이클
         for i in range(1, 6):
-            num = arr[0]
-            arr = arr[1:]
+            num = q[0]
+            q = q[1:]
             num -= i
-            arr.append(num)
+            q.append(num)
 
             # 종료 조건
             if num <= 0:
-                arr[-1] = 0
+                q[-1] = 0
                 break
 
-    print(f'#{tc}', *arr)
+    print(f'#{tc}', *q)
