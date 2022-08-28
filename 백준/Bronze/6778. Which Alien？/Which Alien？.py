2021d3b525518863b1ddb10a6392ed7cfcf35ffa
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-08-28 22:29:58 +0900

    [Bronze IV] Title: Which Alien?, Time: 100 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,26 @@
+a = int(input())
+b = int(input())
+if 3<=a and b<=4:
+
+  print('TroyMartian')
+    
+  if a<=6 and 2<=b:
+
+    print('VladSaturnian')
+  
+  elif a<=2 and b<=3:
+
+    print('GraemeMercurian')
+
+  
+elif a<=6 and 2<=b:
+
+  print('VladSaturnian')
+  
+  if a<=2 and b<=3:
+
+    print('GraemeMercurian')
+
+elif a<=2 and b<=3:
+
+  print('GraemeMercurian')
