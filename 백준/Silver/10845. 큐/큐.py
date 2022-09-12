a3d1e45e6256e78faad5a022384a066e36766df1
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-12 18:09:40 +0900

    [Silver IV] Title: 큐, Time: 184 ms, Memory: 116384 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+n = int(input())
+li = []
+for i in range(n):
+    A = list(input().split())
+    if A[0] == 'push':
+        li.append(int(A[1]))
+    elif A[0] == 'pop':
+        if sum(li) == 0:
+            print('-1\n')
+        else:
+            print(f'{li.pop(0)}\n')
+    elif A[0] == 'size':
+        print(f'{len(li)}\n')
+    elif A[0] == 'empty':
+        if sum(li) == 0:
+            print('1\n')
+        else:
+            print('0\n')
+    elif A[0] == 'front':
+        if sum(li) == 0:
+            print('-1\n')
+        else:
+            print(f'{li[0]}\n')
+    else:
+        if sum(li) == 0:
+            print('-1\n')
+        else:
+            print(f'{li[len(li) - 1]}\n')
