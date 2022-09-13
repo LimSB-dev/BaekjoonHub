e5a2799334749e30527d00295017d39c22a472d4
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-13 14:14:43 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 145 ms, Memory: 56,944 KB -BaekjoonHub

---
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/README.md
@@ -0,0 +1,11 @@
+# [D4] [S/W 문제해결 기본] 9일차 - 중위순회 - 1231 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV140YnqAIECFAYD) 
+
+### 성능 요약
+
+메모리: 56,944 KB, 시간: 145 ms, 코드길이: 780 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -0,0 +1,37 @@
+# 중위 순회
+def inorder_traverse(node):
+    global answer
+
+    if node:
+        inorder_traverse(ch1[node])
+        answer += words[node]
+        inorder_traverse(ch2[node])
+
+
+for tc in range(1, 11):
+    n = int(input())
+    ch1 = [0] * (n + 1)
+    ch2 = [0] * (n + 1)
+    words = [''] * (n + 1)
+    answer = ''
+
+    # 트리 생성
+    for _ in range(n):
+        arr = input().split()
+        idx = int(arr[0])
+        words[idx] = arr[1]
+
+        # 자식 노드가 2개 있을 경우
+        if len(arr) == 4:
+            left, right = int(arr[2]), int(arr[3])
+            ch1[idx] = left
+            ch2[idx] = right
+
+        if len(arr) == 3:
+            left = int(arr[2])
+            ch1[idx] = left
+
+    # 시작 노드를 1로 중위 순회
+    inorder_traverse(1)
+
+    print(f'#{tc} {answer}')
