7321e83136be57d9fcf39ce4cc0be94847531aaa
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-19 12:54:05 +0900

    Algorithm is Fun

--- Algorithm/08_Start_이론/연습문제1. 진수 표현/s1.py
+++ Algorithm/08_Start_이론/연습문제1. 진수 표현/s1.py
@@ -2,6 +2,16 @@ import sys
 sys.stdin = open('input.txt')
 
 for tc in range(1, int(input()) + 1):
-    answer = 0
+    arr = list(map(int, input()))
+    answer = []
 
-    print(f'#{tc} {answer}')
+    while arr:
+        result = 0
+        arr1 = arr[:7]
+        arr = arr[7:]
+        for i in range(7):
+            if arr1[i] == 1:
+                result += 2**(6 - i)
+        answer.append(result)
+
+    print(*answer)
