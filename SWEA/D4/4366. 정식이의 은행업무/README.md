6741ae85e30e25ad2251baf24dc25358a6b8ffcc
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-20 22:38:23 +0900

    [D4] Title: 정식이의 은행업무, Time: 168 ms, Memory: 57,732 KB -BaekjoonHub

---
+++ SWEA/D4/4366. 정식이의 은행업무/README.md
@@ -0,0 +1,11 @@
+# [D4] 정식이의 은행업무 - 4366 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWMeRLz6kC0DFAXd) 
+
+### 성능 요약
+
+메모리: 57,732 KB, 시간: 168 ms, 코드길이: 878 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
@@ -0,0 +1,33 @@
+for tc in range(1, int(input()) + 1):
+    bins = list(map(int, input()))
+    tris = list(map(int, input()))
+    answer = 0
+
+    for i, bi in enumerate(bins):
+        tmp_b = bins[i]
+        bins[i] = (bi + 1) % 2
+
+        for j, tri in enumerate(tris):
+            for k in range(1, 3):
+                b = 0
+                t = 0
+                tmp_t = tris[j]
+                tris[j] = (tri + k) % 3
+
+                for index, value in enumerate(bins[::-1]):
+                    b += value * (2 ** index)
+                for index, value in enumerate(tris[::-1]):
+                    t += value * (3 ** index)
+
+                if b == t:
+                    answer = b
+                    break
+
+                tris[j] = tmp_t
+            if answer != 0:
+                break
+        bins[i] = tmp_b
+        if answer != 0:
+            break
+
+    print(f'#{tc} {answer}')
