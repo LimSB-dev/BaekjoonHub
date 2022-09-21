91305335c396f1534b1e4c60dbccb5f235e8d8c6
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-21 17:33:34 +0900

    Algorithm is Fun

---
+++ Algorithm/09_완전검색:그리디_이론/5188. 최소 합/s2.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+
+    for row in range(n):
+        for col in range(n):
+            r_value = 999999999
+            c_value = 999999999
+            if row != 0:
+                c_value = matrix[row - 1][col]
+            if col != 0:
+                r_value = matrix[row][col - 1]
+
+            value = min(r_value, c_value)
+
+            if value != 999999999:
+                matrix[row][col] += value
+
+    answer = matrix[n - 1][n - 1]
+
+    print(f'#{tc} {answer}')
