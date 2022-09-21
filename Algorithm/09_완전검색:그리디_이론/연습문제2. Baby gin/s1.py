8ad65907ff553cfb002e2619d863642cdcc4b410
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-21 16:45:16 +0900

    Algorithm is Fun

--- Algorithm/09_완전검색:그리디_이론/연습문제2. Baby gin/s1.py
+++ Algorithm/09_완전검색:그리디_이론/연습문제2. Baby gin/s1.py
@@ -1,42 +1,31 @@
+from itertools import permutations
 import sys
 sys.stdin = open('input.txt')
 
 
-def selection_sort(i):
-
-    if i == len(arr) - 1:
-        return
-
-    min_index = i
-
-    for j in range(i + 1, len(arr)):
-        if arr[j] < arr[min_index]:
-            min_index = j
-
-    arr[i], arr[min_index] = arr[min_index], arr[i]
-    selection_sort(i + 1)
-
-
-def baby_gin():
-    global arr, answer
-    if len(arr) >= 3:
-
-        a, b, c = arr[0], arr[1], arr[2]
-
-        if a == b and b == c:
-            arr = arr[3:]
-            baby_gin()
-        elif a + 1 == b and b + 1 == c:
-            arr = arr[3:]
-            baby_gin()
+def baby_gin(array):
+    array = list(array)
+    array.sort()
+    A, B, C = array
+    if (A == B and B == C) or (A + 1 == B and B + 1 == C):
+        return True
+    return False
 
 
 for tc in range(1, int(input()) + 1):
-    arr = list(map(int, input().strip()))
-    selection_sort(0)
-    answer = True
-    baby_gin()
-    if arr:
-        answer = False
+    numbers = list(map(int, input().strip()))
+    answer = False
+    for arr in permutations(numbers, 3):
+        if baby_gin(arr):
+            arr = list(arr)
+            difference = numbers.copy()
 
-    print(answer)
+            for i in arr:
+                if i in difference:
+                    difference.remove(i)
+
+            if baby_gin(difference):
+                answer = True
+                break
+
+    print(f'#{tc} {answer}')
