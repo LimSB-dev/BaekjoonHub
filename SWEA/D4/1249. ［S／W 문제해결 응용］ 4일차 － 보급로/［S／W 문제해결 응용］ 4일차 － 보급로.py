d9c44d62736b19a9eb7d33b4bb92634c4ee057f2
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-09-23 22:15:35 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 271 ms, Memory: 67,400 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+# 상 하 좌 우
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+
+def bfs(row, col, visited, dp):
+
+    # 방문 처리
+    visited[row][col] = True
+
+    # queue 생성
+    queue = deque([[row, col]])
+
+    while queue:
+
+        # dequeue
+        row, col = queue.popleft()
+
+        for direction in range(4):
+            nr = row + dr[direction]
+            nc = col + dc[direction]
+
+            if 0 <= nr < n and 0 <= nc < n:
+                time = dp[row][col] + matrix[nr][nc]
+
+                if not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    dp[nr][nc] = time
+                    queue.append([nr, nc])
+                elif dp[nr][nc] > time:
+                    dp[nr][nc] = time
+                    queue.append([nr, nc])
+
+
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    matrix = [list(map(int, input())) for _ in range(n)]
+    dp = [[0] * n for _ in range(n)]
+    visited = [[False] * n for _ in range(n)]
+
+    bfs(0, 0, visited, dp)
+
+    print(f'#{tc} {dp[n - 1][n - 1]}')
