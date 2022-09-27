95ef0faae6c7ab25bc95bf8b6ed91e18f854ab86
LimSB-dev <mae03087@naver.com>
2022-09-27 12:24:20 +0900

    Algorithm is Fun

--- Algorithm/10_분할정복_실습/5208. 전기 버스 2 (백트래킹)/s1.py
+++ Algorithm/10_분할정복_실습/5208. 전기 버스 2 (백트래킹)/s1.py
@@ -1,22 +1,50 @@
-# 재귀를 이용한 선택정렬
+import sys
+sys.stdin = open('input.txt', encoding='utf-8')
 
-def selection_sort(i):
+def find_min(now, charge):
+    global answer, cnt
 
-    # 종료 조건
-    if i == len(numbers) - 1:
+    # 가지치기
+    if cnt >= answer:
         return
 
-    min_index = i  # min_index를 정렬되지 않는 부분 중 맨 앞 인덱스로 초기화
+    # 현재 위치에서 충전량 만큼 이동했을때 종점 index에 도달하면 종료 조건
+    if now + charge >= station - 1:
+        # 최솟값 비교
+        if cnt < answer:
+            answer = cnt
+        return
 
-    # '맨 앞 인덱스 + 1'부터 비교하여 최소값 인덱스 찾기
-    for j in range(i + 1, len(numbers)):
-        if numbers[j] < numbers[min_index]:
-            min_index = j
+    # 현재 index + 1부터 현재 index + 1 + 충전량만큼 탐색
+    for col in range(now + 1, now + charge + 1):
 
-    numbers[i], numbers[min_index] = numbers[min_index], numbers[i]  # 맨 앞의 값과 최소값을 교환
-    selection_sort(i + 1)  # 재귀 호출
+        # 아직 방문하지 않은 col일 경우
+        if not visited[col]:
+
+            # 방문 갱신
+            visited[col] = True
+
+            charge = arr[col]   # 충전량 변경
+            now = col          # 현재 위치
+            cnt += 1            # 횟수 증가
+
+            find_min(now, charge)
+
+            # 재귀 함수가 바닥까지 가거나 가지치기 당한 후
+            # 다시 now 위치로 돌아와야한다.
+            visited[col] = False
+            now = col
+            cnt -= 1
 
 
-numbers = [5, 2, 7, 1, 3, 8, 9, 3, 5, 2]
-selection_sort(0)  # 0번째 원소부터 정렬 시작
-print(numbers)
+for tc in range(1, int(input()) + 1):
+    arr = list(map(int, input().split()))
+    station = arr[0]
+    arr = arr[1:]
+    visited = [False] * station     # 방문 리스트
+    answer = 100                    # 최솟값
+    cnt = 0                         # 횟수
+
+    find_min(0, arr[0])             # 시작 index / 처음 충전량
+
+    print(f'#{tc} {answer}')
