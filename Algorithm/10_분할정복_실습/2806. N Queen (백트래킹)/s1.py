e2c6d547b326416777b043d656e1b0fcea3dee3b
LimSB-dev <mae03087@naver.com>
2022-09-27 16:37:13 +0900

    Algorithm is Fun

--- Algorithm/10_분할정복_실습/2806. N Queen (백트래킹)/s1.py
+++ Algorithm/10_분할정복_실습/2806. N Queen (백트래킹)/s1.py
@@ -1,22 +1,34 @@
-# 재귀를 이용한 선택정렬
+import sys
+sys.stdin = open('input.txt', encoding='utf-8')
 
-def selection_sort(i):
+
+def dfs(visited, row, post):
+    global answer
 
     # 종료 조건
-    if i == len(numbers) - 1:
+    if row == n:
+        answer += 1
         return
 
-    min_index = i  # min_index를 정렬되지 않는 부분 중 맨 앞 인덱스로 초기화
+    # 열 탐색
+    for col in range(n):
 
-    # '맨 앞 인덱스 + 1'부터 비교하여 최소값 인덱스 찾기
-    for j in range(i + 1, len(numbers)):
-        if numbers[j] < numbers[min_index]:
-            min_index = j
+        # 행, 열 확인
+        if not visited[col] and abs(post - col) != 1:
 
-    numbers[i], numbers[min_index] = numbers[min_index], numbers[i]  # 맨 앞의 값과 최소값을 교환
-    selection_sort(i + 1)  # 재귀 호출
+            visited[col] = True
+
+            dfs(visited, row + 1, col)
+
+            # 가지치기 / 종료조건 이후
+            visited[col] = False
 
 
-numbers = [5, 2, 7, 1, 3, 8, 9, 3, 5, 2]
-selection_sort(0)  # 0번째 원소부터 정렬 시작
-print(numbers)
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    visited = [False] * n
+    answer = 0
+
+    dfs(visited, 0, 999999999)
+
+    print(f'#{tc} {answer}')
--- Algorithm/10_분할정복_실습/5207. 이진 탐색 (분할정복)/s1.py
+++ Algorithm/10_분할정복_실습/5207. 이진 탐색 (분할정복)/s1.py
@@ -2,6 +2,33 @@ import sys
 sys.stdin = open('input.txt', encoding='utf-8')
 
 
+def partition(arr, left, right):
+    pivot = arr[left]
+    i, j = left, right
+
+    while i <= j:
+
+        while i <= j and arr[i] <= pivot:
+            i += 1
+
+        while i <= j and arr[j] >= pivot:
+            j -= 1
+
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[left], arr[j] = arr[j], arr[left]
+
+    return j
+
+
+def quick_sort(arr, left, right):
+    if left < right:
+        mid = partition(arr, left, right)
+        quick_sort(arr, left, mid - 1)
+        quick_sort(arr, mid + 1, right)
+
+
 def find_target(arr, left, right, side):
     global target
 
@@ -12,16 +39,16 @@ def find_target(arr, left, right, side):
 
     target.add(numbers[mid])
 
-    if len(arr) <= 1:
+    if len(arr) == 1:
         return
 
-    left_arr = arr[left:mid]
-    right_arr = arr[mid + 1:right + 1]
+    left_arr = numbers[left:mid]
+    right_arr = numbers[mid + 1:right + 1]
 
-    if side != 1:
+    if side != 1 and left_arr:
         find_target(left_arr, left, mid - 1, 1)
 
-    if side != 2:
+    if side != 2 and right_arr:
         find_target(right_arr, mid + 1, right, 2)
 
 
@@ -30,6 +57,8 @@ for tc in range(1, int(input()) + 1):
     numbers = list(map(int, input().split()))
     targets = set(map(int, input().split()))
     target = set()
+
+    quick_sort(numbers, 0, n - 1)
 
     find_target(numbers, 0, n - 1, 0)
 
