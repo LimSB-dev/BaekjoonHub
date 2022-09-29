3a9da8d3028916b6d01e07cacb23f6e212146393
LimSB-dev <mae03087@naver.com>
2022-09-29 18:00:07 +0900

    Algorithm is Fun

--- Algorithm/11_그래프_실습/1238. 파티/s1.py
+++ Algorithm/11_그래프_실습/1238. 파티/s1.py
@@ -2,53 +2,41 @@ import sys
 sys.stdin = open('input.txt', encoding='utf-8')
 
 
-def pre_order(node):
-    global pre_list
-    if node != 0:
-        pre_list.append(node)
-        pre_order(ch1[node])
-        pre_order(ch2[node])
+def dijkstra(start):
+    visited = [False] * (n + 1)
+    visited[start] = True
+    times[start] = 0
+
+    for end, time in graph[start]:
+        times[end] = time
+
+    for _ in range(n - 1):
+        min_time = INF
+
+        # 최단 시간이 확정되지 않은 정점들 중 최단 시간이 가장 짧은 정점 선택
+        for i in range(1, n + 1):
+            if not visited[i] and min_time > times[i]:
+                min_node = i
+                min_time = times[i]
+
+        visited[min_node] = True
+
+        for next_node, time in graph[min_node]:
+            new_time = times[min_node] + time
+            if new_time < times[next_node]:
+                times[next_node] = new_time
 
 
-def in_order(node):
-    global in_list
-    if node != 0:
-        in_order(ch1[node])
-        in_list.append(node)
-        in_order(ch2[node])
+INF = 999999999
+n, m, x = map(int, input().split())
+times = [INF] * (n + 1)
+graph = [[] for _ in range(n + 1)]
 
+for _ in range(m):
+    s, e, t = map(int, input().split())
+    graph[s].append([e, t])
 
-def post_order(node):
-    global post_list
-    if node != 0:
-        post_order(ch1[node])
-        post_order(ch2[node])
-        post_list.append(node)
+dijkstra(x)
+print(times)
 
-
-v, e = map(int, input().split())
-arr = list(map(int, input().split()))
-
-pre_list = []
-in_list = []
-post_list = []
-
-ch1 = [0] * (v + 1)
-ch2 = [0] * (v + 1)
-
-for i in range(0, e):
-    v1, v2 = arr[i * 2], arr[i * 2 + 1]
-
-    if ch1[v1] == 0:
-        ch1[v1] = v2
-
-    elif ch2[v1] == 0:
-        ch2[v1] = v2
-
-pre_order(1)
-in_order(1)
-post_order(1)
-
-print('전위 순회 :', *pre_list)
-print('중위 순회 :', *in_list)
-print('후위 순회 :', *post_list)
+print(max(times[1:]))
