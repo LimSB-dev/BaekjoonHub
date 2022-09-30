b3e4a98b1cf7527d23b66efd4c0c7edc17662004
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-10-01 08:33:12 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 500 ms, Memory: 151360 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -0,0 +1,52 @@
+from heapq import heappush, heappop
+
+
+def dijkstra(start):
+    distance[start] = 0
+    heap = [(0, start)]
+
+    while heap:
+
+        min_dist, min_node = heappop(heap)
+
+        if min_dist <= distance[min_node]:
+
+            for next_node, dist in graph[min_node]:
+                new_dist = min_dist + dist
+                if new_dist < distance[next_node]:
+                    distance[next_node] = new_dist
+                    heappush(heap, (new_dist, next_node))
+
+
+v, e = map(int, input().split())
+graph = [[] for _ in range(v + 1)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
+    graph[b].append([a, c])
+
+v1, v2 = map(int, input().split())
+
+INF = 999999999
+for i in range(3):
+    distance = [INF] * (v + 1)
+    if i == 0:
+        dijkstra(1)
+        one_v1 = distance[v1]
+        one_v2 = distance[v2]
+    elif i == 1:
+        dijkstra(v1)
+        v1_v2 = distance[v2]
+        v1_n = distance[v]
+    else:
+        dijkstra(v2)
+        v2_v1 = distance[v1]
+        v2_n = distance[v]
+
+answer = min((one_v1 + v1_v2 + v2_n), (one_v2 + v2_v1 + v1_n))
+
+if answer >= INF:
+    answer = -1
+    
+print(answer)
