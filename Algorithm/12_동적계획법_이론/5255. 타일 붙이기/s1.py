acae556c0e7397a7069713e779c78a588a2d8909
LimSB-dev <mae03087@naver.com>
2022-09-30 18:00:29 +0900

    Algorithm is Fun

---
+++ Algorithm/12_동적계획법_이론/5255. 타일 붙이기/s1.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt', encoding='utf-8')
+
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+
+    dp = [0] * (n + 1)
+
+    dp[0]  
+    answer = 0
+
+    print(f'#{tc} {answer}')
