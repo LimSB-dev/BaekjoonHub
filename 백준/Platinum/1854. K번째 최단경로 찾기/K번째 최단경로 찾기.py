992c40120be22a35aca52f81a0250407492620c6
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-10-01 08:32:35 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1024 ms, Memory: 142556 KB -BaekjoonHub

---
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+from heapq import heappush, heappop
+
+
+def dijkstra(start):
+    distance[start][0] = 0
+    heap = [(0, start)]
+
+    while heap:
+
+        min_dist, min_node = heappop(heap)
+
+        for next_node, dist in graph[min_node]:
+            new_dist = min_dist + dist
+            if new_dist < distance[next_node][k - 1]:
+                distance[next_node][k - 1] = new_dist
+                distance[next_node].sort()
+                heappush(heap, (new_dist, next_node))
+
+
+v, e, k = map(int, input().split())
+graph = [[] for _ in range(v + 1)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
+
+INF = 999999999
+distance = [[INF] * k for _ in range(v + 1)]
+dijkstra(1)
+
+answer = []
+for i in range(1, v + 1):
+    if distance[i][k-1] == INF:
+        result = -1
+    else:
+        result = distance[i][-1]
+
+    answer.append(result)
+
+print(*answer, sep='\n')
