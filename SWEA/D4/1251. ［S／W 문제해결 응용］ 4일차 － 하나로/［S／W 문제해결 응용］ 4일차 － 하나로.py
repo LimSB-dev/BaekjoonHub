1f6889c158ca3cf9499cf6b8ccb2312fd253e40a
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-10-01 08:34:09 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 3,565 ms, Memory: 193,168 KB -BaekjoonHub

---
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -0,0 +1,43 @@
+from heapq import heappush, heappop
+
+
+def prim(start):
+    visited = [False] * n
+    heap = [(0, start)]
+    cost = 0
+
+    while heap:
+
+        min_dist, min_node = heappop(heap)
+
+        if not visited[min_node]:
+
+            visited[min_node] = True
+            cost += (min_dist**2) * e
+
+            for next_node, value in enumerate(coordinate):
+                nr, nc = value
+                r, c = coordinate[min_node]
+                dist = ((nr - r)**2 + (nc - c)**2)**0.5
+
+                if not visited[next_node]:
+                    heappush(heap, (dist, next_node))
+
+    return cost
+
+
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+
+    x_arr = list(map(int, input().split()))
+    y_arr = list(map(int, input().split()))
+
+    coordinate = []
+    for col, row in zip(x_arr, y_arr):
+        coordinate.append([row, col])
+
+    e = float(input())
+
+    answer = round(prim(0))
+
+    print(f'#{tc} {answer}')
