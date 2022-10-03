415d916e1ebf767489c764437301e53f00905628
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-10-03 22:23:55 +0900

    Update s1.py

--- Algorithm/11_그래프_실습/1238. 파티/s1.py
+++ Algorithm/11_그래프_실습/1238. 파티/s1.py
@@ -1,42 +1,42 @@
 import sys
 sys.stdin = open('input.txt', encoding='utf-8')
+from heapq import heappush, heappop
 
 
 def dijkstra(start):
-    visited = [False] * (n + 1)
-    visited[start] = True
     times[start] = 0
+    heap = [(0, start)]
 
-    for end, time in graph[start]:
-        times[end] = time
+    while heap:
+        # 1. 시간이 최단 시간이 되는 정점 선택
+        min_time, min_node = heappop(heap)
 
-    for _ in range(n - 1):
-        min_time = INF
+        # 2. 이미 최단 시간으로 기록되어 있는 값보다 짧을 경우
+        if min_time <= times[min_node]:
 
-        # 최단 시간이 확정되지 않은 정점들 중 최단 시간이 가장 짧은 정점 선택
-        for i in range(1, n + 1):
-            if not visited[i] and min_time > times[i]:
-                min_node = i
-                min_time = times[i]
-
-        visited[min_node] = True
-
-        for next_node, time in graph[min_node]:
-            new_time = times[min_node] + time
-            if new_time < times[next_node]:
-                times[next_node] = new_time
+            for next_node, time in graph[min_node]:
+                new_time = min_time + time
+                if new_time < times[next_node]:
+                    times[next_node] = new_time
+                    heappush(heap, (new_time, next_node))
 
 
 INF = 999999999
 n, m, x = map(int, input().split())
-times = [INF] * (n + 1)
 graph = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     s, e, t = map(int, input().split())
     graph[s].append([e, t])
 
-dijkstra(x)
-print(times)
+answer = [0] * (n + 1)
 
-print(max(times[1:]))
+for i in range(1, n + 1):
+    times = [INF] * (n + 1)
+    dijkstra(i)
+    if i == x:
+        answer = [x + y for x, y in zip(answer, times)]
+    else:
+        answer[i] += times[x]
+
+print(max(answer[1:]))
