da90e69dc6cc9568015a350250373d7e59552f16
LimSB-dev <mae03087@naver.com>
2022-10-06 08:50:30 +0900

    Algorithm is Fun

--- Algorithm/12_동적계획법_이론/5255. 타일 붙이기/s1.py
+++ Algorithm/12_동적계획법_이론/5255. 타일 붙이기/s1.py
@@ -3,10 +3,14 @@ sys.stdin = open('input.txt', encoding='utf-8')
 
 for tc in range(1, int(input()) + 1):
     n = int(input())
-
     dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 3
+    dp[3] = 6
 
-    dp[0]  
-    answer = 0
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + (2 * dp[i - 2]) + dp[i - 3]
+
+    answer = dp[n]
 
     print(f'#{tc} {answer}')
