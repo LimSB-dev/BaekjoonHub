8881e99f2df9cf19c6d25e4a18983d12c89257ea
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-10-10 00:14:28 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 220 ms, Memory: 141760 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,21 @@
+while True:
+    n, *numbers = list(map(int, input().split()))
+
+    if n == 0:
+        break
+
+    stack = [0]
+    max_value = 0
+
+    numbers.insert(0, 0)
+    numbers.append(0)
+
+    for i in range(1, n + 2):
+
+        while stack and numbers[stack[-1]] > numbers[i]:
+            idx = stack.pop()
+            max_value = max(max_value, (i - 1 - stack[-1]) * numbers[idx])
+
+        stack.append(i)
+
+    print(max_value)
