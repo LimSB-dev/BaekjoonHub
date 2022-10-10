26c33501a26e07da34766a797a48e20b3a88c4d5
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-10-10 16:58:28 +0900

    [Gold V] Title: 전깃줄, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+lines = [list(map(int, input().split())) for _ in range(n)]
+
+lines.sort(key=lambda x: x[0])
+
+b_line = [b for a, b in lines]
+
+dp = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if b_line[i] > b_line[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
