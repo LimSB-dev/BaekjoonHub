ad1c5e9e951f64c6979ae0a1bf8bab6e3d0a1a7c
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-01 00:19:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 3.83 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,36 @@
+function solution(str1, str2) {
+    const arr1 = makeStr(str1);
+    const arr2 = makeStr(str2);
+   if(arr1.length === 0 && arr2.length === 0) return 65536
+    console.log(arr1, arr2)
+    const same = [];
+    const all = [];
+
+   for(let i = 0 ; i < arr1.length ; i ++) {
+       if(arr2.includes(arr1[i])) {
+           const idx = arr2.indexOf(arr1[i]);
+           arr2.splice(idx,1);
+           same.push(arr1[i]);
+       } else {
+           all.push(arr1[i])
+       }
+   }
+
+    arr2.forEach((e) => all.push(e));
+
+    const result = Math.floor((same.length / (same.length+all.length)) * 65536)
+    return result
+}
+
+
+function makeStr(array) {
+    const arr = [];
+    const regexp = /[\W0-9_]/
+    array.split('')
+    for(let i = 1 ; i < array.length; i ++) {
+        const elem = array[i-1] + array[i];
+        if(regexp.test(elem)) continue;
+        arr.push(elem.toLowerCase());
+    }
+    return arr
+}
