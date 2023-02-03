caa163b4d1ebdb45ad7d0d6bac0bf2ef6a3ed307
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-03 19:35:26 +0900

    [unrated] Title: 무인도 여행, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
@@ -0,0 +1,67 @@
+// 상 하 좌 우
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+function solution(maps) {
+    let answer = []
+    let matrix = []
+    
+    maps.forEach(row => {
+        matrix.push(row.split(''))
+    })
+    
+    const [row, col] = [matrix.length - 1, matrix[0].length - 1]
+    
+    for (let i = 0; i <= row; i++) {
+        for (let j = 0; j <= col; j++) {
+            let value = matrix[i][j]
+            
+            // 방문 확인
+            if (value === 'X') continue
+            
+            // 방문 처리
+            matrix[i][j] = 'X'
+            
+            value = Number(value)
+            
+            // 큐 생성
+            const queue = [[i, j, value]]
+
+            while(queue.length !== 0) {
+
+                // 큐 추출
+                let [r, c, v] = queue.shift()
+                
+                // 상 하 좌 우 확인
+                for(let i = 0; i < 4; i++) {
+                    const [nr, nc] = [r + dr[i], c + dc[i]]
+                    
+                    // 범위 밖으로 나갈 경우
+                    if (0 > nr || nr > row || 0 > nc || nc > col) continue
+
+                    let nv = matrix[nr][nc]
+                    
+                    // 방문 확인
+                    if (nv === 'X') continue
+
+                    // 방문 처리
+                    matrix[nr][nc] = 'X'
+                    nv = Number(nv)
+                    
+                    value += nv
+                    
+                    // 큐 추가       
+                    queue.push([nr, nc, nv])
+                }
+            }
+            
+            answer.push(value)
+        }
+    }
+    
+    if (answer.length === 0) return [-1]
+    
+    answer.sort((a, b) => a - b)
+    
+    return answer
+}
