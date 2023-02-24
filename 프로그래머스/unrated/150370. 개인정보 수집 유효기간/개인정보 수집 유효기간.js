6eeeebfa8743ec0cca75748fa3e1eff7a62cfeed
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-02-24 10:54:35 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,49 @@
+/** 약관 판단 후 유효기간을 지나 파기되어야 한다면 ture 반환 */
+function check(curYear, curMonth, curDay, year, month, day, value) {
+    month += value
+    
+    if (month > 12) {
+        year += Math.floor(month / 12);
+        month = month % 12;
+    }
+    
+    if (month === 0) {
+        month = 12
+        year--
+    }
+    
+    if (curYear !== year) {
+        return curYear > year
+    }
+
+    if (curMonth !== month) {
+        return curMonth > month
+    }
+
+    return curDay >= day
+}
+
+function solution(today, terms, privacies) {
+    let answer = []
+    const [curYear, curMonth, curDay] = today.split('.').map(string => Number(string))
+    let expiration = new Map()
+    
+    terms.map(term => {
+        const [key, value] = term.split(' ')
+        
+        expiration[key] = Number(value)
+    })
+    
+    privacies.map((privacie, idx) => {
+        const [date, key] = privacie.split(' ')
+        const [year, month, day] = date.split('.').map(string => Number(string))
+        const value = expiration[key]
+        
+        // 유효기간 확인
+        if (check(curYear, curMonth, curDay, year, month, day, value)) {
+            answer.push(idx + 1)
+        }
+    })
+    
+    return answer
+}
