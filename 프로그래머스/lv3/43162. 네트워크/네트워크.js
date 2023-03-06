3704fb898caf53a752db43b5d7880a0847ac6bdb
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-03-06 10:33:42 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,24 @@
+function solution(n, computers) {
+    let answer = 0
+    let visited = new Array(n).fill(false)
+    
+    // DFS 함수
+    const dfs = (computers, visited, idx) => {
+        visited[idx] = true;
+        for (let i = 0; i < computers.length; i++) {
+            if (computers[idx][i] === 1 && !visited[i]) {
+                dfs(computers, visited, i);
+            }
+        }
+    }  
+    
+    // 모든 컴퓨터에 대해 DFS 수행
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(computers, visited, i);
+            answer++;
+        }
+    }
+    
+    return answer
+}
