404d7394dbdfa2984956a81b561fa3c683de6b15
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-03-18 21:22:13 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 2620 ms, Memory: 79416 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.py
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.py
@@ -1,25 +1,33 @@
 import sys
 # sys.stdin = open('input.txt')
 
-def is_palindrome(s):
-    return s == s[::-1]
-
 s = sys.stdin.readline().strip()
 n = len(s)
 
-dp = [0] * n
+dp = [[False] * n for _ in range(n)]
 for i in range(n):
-    min_cut = i + 1  # 문자열의 길이보다 큰 값을 초기값으로 설정
+    dp[i][i] = True
+
+for i in range(n - 1):
+    if s[i] == s[i + 1]:
+        dp[i][i + 1] = True
+
+for k in range(3, n + 1):
+    for i in range(n - k + 1):
+        j = i + k - 1
+        if s[i] == s[j] and dp[i + 1][j - 1]:
+            dp[i][j] = True
+
+dp2 = [0] * n
+for i in range(n):
+    min_cut = i
     for j in range(i + 1):
-        if is_palindrome(s[j:i + 1]):
+        if dp[j][i]:
             if j == 0:
-                # j가 0인 경우는 문자열의 시작부터 팰린드롬인 경우이므로,
-                # 최소 팰린드롬 분할 개수는 1이다.
-                min_cut = 1
+                min_cut = 0
                 break
             else:
-                # j가 0이 아닌 경우, j-1까지의 최소 팰린드롬 분할 개수에 1을 더한다.
-                min_cut = min(min_cut, dp[j - 1] + 1)
-    dp[i] = min_cut
+                min_cut = min(min_cut, dp2[j - 1] + 1)
+    dp2[i] = min_cut
 
-print(dp[n - 1])
+print(dp2[n - 1] + 1)
