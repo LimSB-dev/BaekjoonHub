aae18f261edef8b7666bd5e08c685552f0050b08
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-05-27 12:17:25 +0900

    [Silver III] Title: 블로그2, Time: 96 ms, Memory: 39168 KB -BaekjoonHub

---
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+problems = list(input().strip())
+
+problems = [problems[0]] + [problems[i]
+                            for i in range(1, N) if problems[i] != problems[i-1]]
+
+if len(problems) == 1:
+    print(1)
+else:
+    second_color_cnt = problems.count(problems[1])
+
+    print(1 + second_color_cnt)
+
+
+# 아이디어 1. 연속된 색은 하나로 생각하기.
+# 아이디어 2. 가장 빠른 방법은 시작 색을 다 칠하고 나머지 색을 칠하는 것.
