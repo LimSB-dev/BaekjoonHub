bf6df4440bfd08c10eb5b3ee54e212bdb5dffe03
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-13 19:31:34 +0900

    [Gold V] Title: 트리, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,18 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(delete_node, tree):
+def dfs(delete_node):
     tree[delete_node] = -2
     for node in range(n):
         if delete_node == tree[node]:
-            dfs(node, tree)
+            dfs(node)
 
 n = int(input())
 tree = list(map(int, input().split()))
 delete_node = int(input())
 leaf = 0
 
-dfs(delete_node, tree)
+dfs(delete_node)
 
 for node in range(n):
     if tree[node] != -2 and node not in tree:
