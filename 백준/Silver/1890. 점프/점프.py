5a9b0a4cbedc6d2f8ab2617a6b11a0ba2cb88aae
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-19 10:03:18 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = 1
+
+for row in range(N):
+    for col in range(N):
+        if row == N - 1 and col == N - 1:
+            break
+        if dp[row][col] > 0:
+            jump = matrix[row][col]
+            if row + jump < N:
+                dp[row + jump][col] += dp[row][col]
+            if col + jump < N:
+                dp[row][col + jump] += dp[row][col]
+
+print(dp[-1][-1])
