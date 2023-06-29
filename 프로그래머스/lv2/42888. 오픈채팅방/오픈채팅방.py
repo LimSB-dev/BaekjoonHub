1e8382fe9bc320e3b3fac3f138334cd2b2fee3ac
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-06-29 10:22:06 +0900

    [level 2] Title: 오픈채팅방, Time: 94.24 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -2,37 +2,21 @@ def solution(record):
     n = len(record)
     answer = []
     name_dict = dict()
-    
-    def add_user(uid, name):
-        if name_dict.get(uid):
+    printer = {"Enter": "님이 들어왔습니다.", "Leave": "님이 나갔습니다."}
+
+    def add_user(action, uid, name = "홍길동"):
+        if action in ["Enter", "Change"]:
             name_dict[uid] = name
-        else:
-            name_dict.setdefault(uid, name)
-    
-    
+
+
     def add_chat(action, uid, name = "홍길동"):
-        if action == "Enter":
-            saved_name = name_dict.get(uid)
-            if saved_name:
-                answer.append(f"{saved_name}님이 들어왔습니다.")
-            else:
-                name_dict.setdefault(uid, name)
-                answer.append(f"{name}님이 들어왔습니다.")
-        elif action == "Leave":
-            saved_name = name_dict.get(uid)
-            if saved_name:
-                answer.append(f"{saved_name}님이 나갔습니다.")
-            else:
-                answer.append(f"{name}님이 나갔습니다.")
-        elif action == "Change":
-            return
-        else:
-            return    
-    
+        if action != "Change":
+            answer.append(f"{name_dict[uid]}{printer[action]}")
+
+
     for i in range(n):
         arr = record[i].split()
-        if len(arr) == 3:
-            add_user(arr[1], arr[2])
+        add_user(*arr)
     
     for i in range(n):
         arr = record[i].split()
