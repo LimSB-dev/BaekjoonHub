c32d663b1615af136b56dace698787802a016649
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-07-07 13:21:52 +0900

    [Silver I] Title: 카잉 달력, Time: 1536 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,12 +1,18 @@
 import sys
+import math
 input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
     M, N, x, y = map(int, input().split())
+    
+    # x와 y에 대한 최소공배수를 구합니다.
+    lcm = M * N // math.gcd(M, N)
+    
     answer = -1
-    for i in range(x, M*N+1, M):
-        if i % N == y % N:
+    
+    for i in range(x, lcm+1, M):
+        if (i - 1) % N + 1 == y:
             answer = i
             break
 
