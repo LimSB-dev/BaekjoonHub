897aaf3d86bc344fa1134723861a8d5463441831
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-07-14 00:42:10 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 27.97 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,18 +1,13 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    
     delivery = 0
     pickup = 0
-
     for i in range(n-1, -1, -1):
-
         if deliveries[i] != 0 or pickups[i] != 0:
-            
             while delivery < deliveries[i] or pickup < pickups[i]:
                 delivery += cap
                 pickup += cap
                 answer += (i + 1) * 2
             delivery -= deliveries[i]
             pickup -= pickups[i] 
-
     return answer
