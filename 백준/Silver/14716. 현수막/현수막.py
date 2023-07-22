b6afc8cd8cf91d02247e1116474d1da2df95eb84
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-07-22 15:31:14 +0900

    [Silver I] Title: 현수막, Time: 244 ms, Memory: 32676 KB -BaekjoonHub

---
+++ 백준/Silver/14716. 현수막/현수막.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+answer = 0
+m, n = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(m)]
+visited = [[False] * n for _ in range(m)]
+direction = [
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1), (0, 1),
+    (1, -1), (1, 0), (1, 1)
+]
+
+
+for row in range(m):
+    for col in range(n):
+        if matrix[row][col] == 1 and not visited[row][col]:
+            answer += 1
+            stack = [(row, col)]
+            visited[row][col] = True
+            while stack:
+                r, c = stack.pop()
+                for dr, dc in direction:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n and not visited[nr][nc]:
+                        visited[nr][nc] = True
+                        if matrix[nr][nc] == 1:
+                            stack.append((nr, nc))
+
+print(answer)
