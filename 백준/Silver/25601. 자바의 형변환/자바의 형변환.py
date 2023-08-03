07606e672282f3851775249e402729c7ea336759
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-08-03 09:44:38 +0900

    [Silver I] Title: 자바의 형변환, Time: 160 ms, Memory: 51392 KB -BaekjoonHub

---
+++ 백준/Silver/25601. 자바의 형변환/자바의 형변환.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = {}
+for _ in range(n-1):
+    child, parent = input().split()
+    graph[child] = parent
+
+a, b = input().split()
+answer = 0
+
+
+def check_casting(child, parent):
+    while child != parent:
+        if graph.get(child) is None:
+            return False
+        child = graph[child]
+
+    return True
+
+
+if check_casting(a, b) or check_casting(b, a):
+    answer = 1
+
+print(answer)
