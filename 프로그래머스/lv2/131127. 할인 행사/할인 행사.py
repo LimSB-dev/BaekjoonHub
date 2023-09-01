4a822697281b401e7dcaa489f17c32f809f9a06b
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-09-01 15:48:45 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,14 @@
+from copy import deepcopy
+
+def solution(want, number, discount):
+    answer = 0
+    w = len(want)
+    d = len(discount)
+    want_list = sorted([want[i] for i in range(w) for _ in range(number[i])])
+    
+    for i in range(d - 9):
+        cur_want = sorted(discount[i:i+10])
+        if cur_want == want_list:
+            answer += 1
+        
+    return answer
